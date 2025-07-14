c351266743fe953994e96a36764d9ce035e65d46
Katherin Guerrero <kathejudith@gmail.com>
2025-07-14 08:29:00 -0300

    inicio, servicios y sobre mi, completado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://portfolio-kathe.netlify.app/",
-  integrations: [preact()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 });
